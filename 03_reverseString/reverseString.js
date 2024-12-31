99d036b43bbf2958a0257b595adb23781ccae4ad
Mihir <mihirsingh@berkeley.edu>
2024-12-30 18:53:54 -0800

    completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let characters = input.split(""); //if input = 'hello', characters = ['h', 'e', 'l', 'l', 'o']
+    let returnString = '';
 
+    for (let i = characters.length - 1; i >= 0; i--) {
+        returnString += characters[i];
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
