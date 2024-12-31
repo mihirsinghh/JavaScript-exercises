462b5be824634d2c8cdae0c19c39921bc5e5abed
Mihir <mihirsingh@berkeley.edu>
2024-12-30 18:38:32 -0800

    completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnVal = '';
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < num; i++) {
+            returnVal += string;
+        }
+        return returnVal;
+    }
 };
 
 // Do not edit below this line
