cb9c2c84f4ee67e32ab202dd2f6b3bf074733397
Mihir <mihirsingh@berkeley.edu>
2024-12-31 16:39:30 -0800

    completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if (typeof(num1) != 'number' || !Number.isInteger(num1) || !Number.isInteger(num2) || typeof(num2) != 'number' || num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+
+    if (num2 > num1) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = num2; i <= num1; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
