7cb878f3e40c4067ca3aead934a0bba66fdc88aa
Mihir <mihirsingh@berkeley.edu>
2024-12-31 17:02:33 -0800

    completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let temp = (tempInF - 32) * 5/9;
+
+  return parseFloat(temp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  let temp = (tempInC * 9/5) + 32;
+
+  return parseFloat(temp.toFixed(1));
 };
 
 // Do not edit below this line
