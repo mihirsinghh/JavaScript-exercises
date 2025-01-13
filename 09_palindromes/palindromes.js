a340c664364088ac25fbc1eaa9523691d8b75519
Mihir <mihirsingh@berkeley.edu>
2025-01-13 14:27:38 -0800

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanStr = '';
 
+    for (let char of str) {
+        if (
+            (char >= 'a') && (char <= 'z') ||
+            (char >= 'A') && (char <= 'Z') ||
+            (char >= '0') && (char <= '9') 
+        ) {
+            cleanStr += char.toLowerCase();
+        }
+    }
+
+    //split the string into an array of individual characters, reverse the array, then recombine
+    //the array elements into a single, continuous string of characters
+    const reversedStr = cleanStr.split('').reverse().join('');
+    return cleanStr === reversedStr;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
