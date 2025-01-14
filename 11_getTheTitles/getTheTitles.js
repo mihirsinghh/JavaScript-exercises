cd5aab33ba817e0b16df3651afaf5678614cfd4a
Mihir <mihirsingh@berkeley.edu>
2025-01-14 09:41:08 -0800

    completed 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titleArr = [];
+    for (const obj of arr) {
+        titleArr.push(obj.title);
+    }
+    return titleArr;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
