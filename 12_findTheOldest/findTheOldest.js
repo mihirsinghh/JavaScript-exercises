5d8194e5c4192b6974d263e205f53c59e9511c2e
Mihir <mihirsingh@berkeley.edu>
2025-01-14 10:02:10 -0800

    completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    let oldestPersonAge = 0;
+    let eachPersonAge = 0;
 
+    for (const eachPerson of peopleArr) {
+        if (eachPerson.yearOfDeath == null) {
+            const now = new Date();
+            eachPersonAge = now.getFullYear() - eachPerson.yearOfBirth;
+        } else {
+            eachPersonAge = eachPerson.yearOfDeath - eachPerson.yearOfBirth;
+        }
+
+        if (eachPersonAge > oldestPersonAge) {
+            oldestPersonAge = eachPersonAge;
+        }
+    }
+
+    for (const eachPerson of peopleArr) {
+        if (eachPerson.yearOfDeath == null) {
+            const now = new Date();
+            let personAge = now.getFullYear() - eachPerson.yearOfBirth;
+            if (personAge === oldestPersonAge) {
+                return eachPerson;
+            }
+        } else {
+            let personAge = eachPerson.yearOfDeath - eachPerson.yearOfBirth;
+            if (personAge === oldestPersonAge) {
+                return eachPerson;
+            }
+        }
+    }
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
